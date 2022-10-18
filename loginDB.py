7a5568e09d403ab7aa8f0a96de9eb53887f8344c
Saleh Ahmad <salehahmad625@gmail.com>
2022-10-18 16:50:21 -0500

    register and verify login functions added

--- loginDB.py
+++ loginDB.py
@@ -1,13 +1,42 @@
 from pymongo import MongoClient
 
+
 def registerUser(username, password):
     connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
     Client = MongoClient(connection_string)
+    db = Client["Passwords"]
+    encryptedUser = encrypt(username)
+    encryptedPass = encrypt(password)
+    alrExists = db.list_collection_names().__contains__(encryptedUser)
+    if alrExists:
+        print("User already exists. Choose another name.")
+        return False
+    else:
+        db.create_collection(encryptedUser)
+        doc = {"Password": encryptedPass}
+        col = db[encryptedUser]
+        col.insert_one(doc)
+        return True
 
 
 def verifyLogin(username, password):
     connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
     Client = MongoClient(connection_string)
+    db = Client["Passwords"]
+    encryptedUser = encrypt(username)
+    encryptedPass = encrypt(password)
+    inDB = db.list_collection_names().__contains__(encryptedUser)
+    if not inDB:
+        print("Invalid username.")
+        return False
+    else:
+        col = db[encryptedUser]
+        passArr = col.find_one({}, {"_id": 0})
+        actualPass = passArr["Password"]
+        if actualPass == encryptedPass:
+            return True
+        else:
+            return False
 
 
 def encrypt(word):
