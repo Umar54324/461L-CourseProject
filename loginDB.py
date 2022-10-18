c6f5637330cfa06426883507116c24070dec0e93
Saleh Ahmad <salehahmad625@gmail.com>
2022-10-18 16:08:13 -0500

    added encrypt & decrypt functions

--- loginDB.py
+++ loginDB.py
@@ -1,2 +1,26 @@
 from pymongo import MongoClient
 
+def registerUser(username, password):
+    connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
+    Client = MongoClient(connection_string)
+
+
+def verifyLogin(username, password):
+    connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
+    Client = MongoClient(connection_string)
+
+
+def encrypt(word):
+    encrypted = ""
+    for i in range(word.__len__()-1, -1, -1):
+        encrypted += chr(ord(word[i])+7)
+    return encrypted
+
+
+def decrypt(word):
+    original = ""
+    for i in range(word.__len__()-1, -1, -1):
+        original += chr(ord(word[i])-7)
+    return original
+
+
