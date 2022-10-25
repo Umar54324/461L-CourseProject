4cd11780e8b48299af039bc1aefe5ccc268c147f
Umar Choudry <umarchoudrysonic@gmail.com>
2022-10-25 17:18:27 -0500

    All Backend Functions Tested and Working

--- stockDB.py
+++ stockDB.py
@@ -1,39 +1,40 @@
 from pymongo import MongoClient
-
+import certifi
+ca = certifi.where()
 
 def checkInItem(item, set_name, qty):    # assuming item already exists, check it back in
     connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
-    Client = MongoClient(connection_string)
+    Client = MongoClient(connection_string, tlsCAFile =ca)
     db = Client["Stock"]
     col = db[set_name]
     data = col.find_one({"Item": item})
     init_avail = data["Availability"]
-    col.update_one({"Item": item}, {"$set": {"Availability": init_avail+qty}})
+    col.update_one({"Item": item}, {"$set": {"Availability": int(init_avail)+int(qty)}})
     # update userDB HWSet item quantity
     # check to make sure its less than capacity? is that necessary?
 
 
 def checkOutItem(item, set_name, qty):
     connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
-    Client = MongoClient(connection_string)
+    Client = MongoClient(connection_string, tlsCAFile =ca)
     db = Client["Stock"]
     col = db[set_name]
     data = col.find_one({"Item": item})
     init_avail = data["Availability"]
-    if init_avail - qty < 0:
+    if int(init_avail) - int(qty) < 0:
         col.update_one({"Item": item}, {"$set": {"Availability": 0}})
         return init_avail
         # update userDB HWSet quantity
 
     else:
-        col.update_one({"Item": item}, {"$set": {"Availability": init_avail - qty}})
+        col.update_one({"Item": item}, {"$set": {"Availability": int(init_avail) - int(qty)}})
         return 0
         # update userDB HWSet quantity
 
 
 def addNewItem(set_name, item, capacity, availability):
     connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
-    Client = MongoClient(connection_string)
+    Client = MongoClient(connection_string, tlsCAFile =ca)
     db = Client["Stock"]
     col = db[set_name]
     doc = {"Item": item, "Availability": availability, "Capacity": capacity}
@@ -42,7 +43,7 @@ def addNewItem(set_name, item, capacity, availability):
 
 def removeItem(set_name, item):
     connection_string = "mongodb+srv://salehahmad:rMbinVQqIZXr9fSS@deskupcluster.mifqwta.mongodb.net/test"
-    Client = MongoClient(connection_string)
+    Client = MongoClient(connection_string, tlsCAFile =ca)
     db = Client["Stock"]
     col = db[set_name]
     col.delete_one({"Item": item})
